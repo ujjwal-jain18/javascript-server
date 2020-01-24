86a623c5a5ecfac09343f2d32d988683e2eefebe
ujjwal-jain18 <ujjwal.jain@successive.tech>
2020-01-24 20:36:08 +0530

    docs:add validation and permissions

--- extra/utils/permission.js
+++ extra/utils/permission.js
@@ -8,13 +8,14 @@ delete: [],
 }
 
 };
-function hasPermission(moduleName, role, permissionType)
-{
-let data=permissions[moduleName];
-let x=data[permissionType];
-return x.some(element => {if (element===role)
-return true;
-else
-return false});
+
+ function hasPermission(moduleName, role, permissionType) {
+    let data = permissions[moduleName];
+    let x = data[permissionType];
+    return x.some(element => {if (element === role)
+    return true;
+    else
+    return false});
 }
+
 console.log(hasPermission("getUsers","trainee","read"));
