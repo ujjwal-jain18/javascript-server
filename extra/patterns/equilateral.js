3b74210223a9344cc27952f9d430d494757a6546
ujjwal-jain18 <ujjwal.jain@successive.tech>
2020-01-24 09:49:54 +0530

    docs:add package.json and patterns

--- extra/patterns/equilateral.js
+++ extra/patterns/equilateral.js
@@ -1,19 +1,26 @@
 var x=Number(process.argv[2]);
 equilateral(x);
+
 function equilateral (x)
-{ console.log("print a equilateral triangle with rows "+x);
-    if(x<2&&x>10)
-    return(0);
-for(i=0;i<=x;i++)
-{ a="";
+{
+    console.log("print a equilateral triangle with rows "+x);
+   
+    if(x<2 && x>10)
+       return(0);
+   
+    for(i=0;i<=x;i++)
+     {
+          a="";
+
     for(j=i;j<=x;j++)
-        a+=" ";
+          a+=" ";
 
     for (let z=i;z>0;z--)
-        a+="* ";
-        console.log(a);
+          a+="* ";
+    
+    console.log(a);
 
-}
+     }
 
 }
 
