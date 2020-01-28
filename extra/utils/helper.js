c05893c4436dee4fd2ab334c8a70dd8745c62297
ujjwal-jain18 <ujjwal.jain@successive.tech>
2020-01-28 11:50:07 +0530

    feat:add packages

--- extra/utils/helper.js
+++ extra/utils/helper.js
@@ -1,15 +1,10 @@
-let valid=[];
-let invalid=[];
+
 function validateEmail(a){
     
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((successive.tech))$/;
     
    let  match = re.test(a);
 
-    if( match == true)  valid.push(a);
-
-    else invalid.push(a);
- 
     return(match);
 }
 export {validateEmail};
