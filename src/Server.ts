e11ca740e9abcca38595967314c56c27dbd592ff
ujjwal-jain18 <ujjwal.jain@successive.tech>
2020-01-30 15:44:15 +0530

    # feat:add http handler

--- src/Server.ts
+++ src/Server.ts
@@ -1,6 +1,8 @@
 import * as express from 'express';
 import Iconfig from './config/Iconfig';
-
+import * as bodyParser from 'body-parser';
+import notFound from './libs/Routes/notFoundRoute';
+import ErrorHandler from './libs/Routes/ErrorHandler';
 class Server {
   app: express.Application;
   constructor(private config: Iconfig) {
@@ -8,13 +10,23 @@ class Server {
   }
   bootstrap(): Server  {
     this.setupRoutes();
+    this.BodyParser();
     return this;
   }
+   BodyParser(): void {
+   // parse application/x-www-form-urlencoded
+   this.app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+   this.app.use(bodyParser.json());
+  }
+
   setupRoutes(): void {
     this.app.get('/health-check', (req: express.Request, res: express.Response) => {
-        res.send('I am OK');
-      }
-    );
+      res.send('I am OK');
+    }
+  );
+    this.app.use(notFound);
+    this.app.use(ErrorHandler);
   }
   run(): Server {
     this.app.listen(this.config.port, (err: any) => {
---
+++ src/libs/Routes/ErrorHandler.ts
@@ -0,0 +1,10 @@
+const ErrorHandler: any = (err: any , req: any, res: any, next: any) => {
+    console.log('Error', err);
+    res.send({
+        error: 'Not Found',
+        message: 'error',
+        status: 500,
+        timestamp: new Date()
+});
+};
+ export default ErrorHandler;
